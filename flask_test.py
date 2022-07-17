00475dbcfa7e8391b59f947ed4e0712770588254
matatkos <matatkos@gmail.com>
2022-07-17 19:59:31 +0200

    Working on flask api

--- flask_test.py
+++ flask_test.py
@@ -1,53 +1,70 @@
 import requests
-import numbers
 import json
 from flask import Flask, request, render_template, jsonify
 from flask_restful import Resource, Api
-from requests import put, get
 from flask.views import MethodView, View
-from flask_sqlalchemy import SQLAlchemy
+from peewee import SqliteDatabase, Model, TextField, IntegerField
+import os
+from os.path import exists
+if os.path.exists('posts.db'):
+    os.remove('posts.db')
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.database'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 base_url = 'http://localhost:3000/'
 
 users_endpoint = 'users'
 posts_endpoint = 'posts'
 
-db=SQLAlchemy(app)
+db = SqliteDatabase('posts.db')
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer)
-    title= db.Column(db.String)
-    body= db.Column(db.String)
+class BaseTable(Model):
+    class Meta:
+        database = db
 
-db.create_all()
+class Post(BaseTable):
+    id = IntegerField(primary_key=True, index=True)
+    userId = IntegerField()
+    title= TextField()
+    body= TextField()
 
+db.connect()
+db.create_tables([Post])
 
-
+'''post1 = Posts.create(
+    userId= 1,
+    title = "Skusobny text na peewee",
+    body="Testujem peewee"
+)
+post1.save()
+for posts in Posts.select():
+    print("id:",posts.id)
+'''
 Error_Wrong_ID_Int = "Wrong ID input. ID has to be integer."
 Error_Wrong_Data_Str = "Wrong data input. Data have to be string."
 Error_Data_Missing = "Some data you want to input are missing. Please check again."
 Error_Post_Not_Found = "Post with this ID could not be found."
 Error_ID_Not_Input = "ID or UserID is missing."
 
-def get_post_by_id(id, posts):
-    for i in posts:
-        if i['id'] == id:
-            return i
-
-def get_post_by_userid(userid, posts):
-    for i in posts:
-        if i['userId'] == userid:
-            return i
 
 def get_posts():
     r = requests.get(base_url+posts_endpoint)
     posts = json.loads(r.content)
     return posts
+
+posts = get_posts()
+
+for post in posts:
+    new_post = Post.create(
+        id= post['id'],
+        userId = post['userId'],
+        title = post['title'],
+        body = post['body']
+    )
+    print(new_post.id, new_post.userId, new_post.title, new_post.body)
+    new_post.save()
+
 
 
 
@@ -59,18 +76,21 @@ def home():
 #class Users(View)
 class Posts(MethodView):
     def get(self, id : str, userId : str):
-
         posts = get_posts()
         #If there is ID
         if id is not None:
-
             if id.isnumeric():
                 id=int(id)
-                for i in posts:
-                    if i['id'] == id:
-                        user_posts = []
+                user_posts = []
+                for i in Post.select():
+                    if i.id == id:
                         user_posts.append(i)
                         return render_template('posts.html', posts=user_posts)
+                if user_posts == None:
+                    for i in posts:
+                        if i['id'] == id:
+                            user_posts.append(i)
+                            return render_template('posts.html', posts=user_posts)
                 return render_template('errors.html', error= Error_Post_Not_Found)
             return render_template('errors.html', error=Error_Wrong_ID_Int)
         #If there is UserID
@@ -79,12 +99,19 @@ class Posts(MethodView):
                 userId=int(userId)
                 user_posts = []
                 any_posts = False
-                for i in posts:
-                    if i['userId'] == userId:
+                for i in Post.select():
+                    if i.userId == userId:
                         user_posts.append(i)
                         any_posts= True
                 if any_posts:
                     return render_template('posts.html', posts=user_posts)
+                else:
+                    for i in posts:
+                        if i['userId'] ==userId:
+                            user_posts.append(i)
+                            any_posts=True
+                    if any_posts == True:
+                        return render_template('posts.html', posts=user_posts)
                 return render_template('errors.html', error=Error_Post_Not_Found)
             return render_template('errors.html', error=Error_Wrong_ID_Int)
         #Showing all the posts
@@ -98,15 +125,21 @@ class Posts(MethodView):
         if new_post_body is None or new_post_id is None or new_post_userId is None or new_post_body is None:
             return render_template('errors.html', error=Error_Data_Missing)
 
-        post= Post(new_post_id, new_post_userId, new_post_title, new_post_body)
-        db.session.add(post)
-        db.commit()
+        for i in Post.select():
+            if i.id == new_post_id:
+                return render_template('errors.html', error= "There is already post with this ID")
 
-        new_post = {'id': new_post_id, 'userId': new_post_userId, 'title': new_post_title, 'body': new_post_body}
+        new_post = Post.create(
+            id =new_post_id,
+            userId = new_post_userId,
+            title = new_post_title,
+            body= new_post_body
+        )
+        new_post.save()
+        new_post = {'id': new_post.id, 'userId': new_post.userId, 'title': new_post.title, 'body': new_post.body}
 
         with open('db.json', 'r') as f:
             feeds = json.load(f)
-            print(feeds)
         with open('db.json', 'w') as f:
             feeds["posts"].append(new_post)
             json.dump(feeds, f, indent = 4)
@@ -115,6 +148,10 @@ class Posts(MethodView):
     def put(self, id : str, userId : str):
         new_title = request.json['title']
         new_body = request.json['body']
+
+        if new_title is None or new_body is None:
+            return render_template('errors.html', error=Error_Data_Missing)
+
         if isinstance(new_title, str) == False or isinstance(new_body,str) == False:
             return render_template('errors.html', error=Error_Wrong_Data_Str)
 
@@ -124,58 +161,80 @@ class Posts(MethodView):
         if id is not None:
             if id.isnumeric():
                 id= int(id)
-                posts = feeds['posts']
-                for i in posts:
-                    if i['id'] == id:
-                        if new_title is not None:
-                            i['title'] = new_title
-                        if new_body is not None:
-                            i['body'] = new_body
-                        with open('db.json', 'w') as f:
-                            json.dump(feeds, f, indent=4)
-                        return i
+                for i in Post.select():
+                    if i.id == id:
+                        i.title = new_title
+                        i.body = new_body
+                        posts = feeds['posts']
+                        for j in posts:
+                            if j['id'] == i.id:
+                                if new_title is not None:
+                                    i['title'] = i.title
+                                if new_body is not None:
+                                    i['body'] = i.body
+                                with open('db.json', 'w') as f:
+                                    json.dump(feeds, f, indent=4)
+                                return i
                 return render_template('errors.html', error=Error_Post_Not_Found)
             return render_template('errors.html', error=Error_Wrong_ID_Int)
-
         elif userId is not None:
             if userId.isnumeric():
                 userId=int(userId)
-                posts = feeds['posts']
-                for i in posts:
-                    if i['userId'] == userId:
-                        if new_title is not None:
-                            i['title'] = new_title
-                        elif new_body is not None:
-                            i['body'] = new_body
-                        with open('db.json', 'w') as f:
-                            json.dump(feeds, f, indent=4)
-                        return i
+                for j in Post.select():
+                    if j.userId == userId:
+                        j.title = new_title
+                        j.body = new_body
+                        posts = feeds['posts']
+                        for i in posts:
+                            if i['userId'] == j.userId:
+                                if new_title is not None:
+                                    i['title'] = j.new_title
+                                elif new_body is not None:
+                                    i['body'] = j.new_body
+                                with open('db.json', 'w') as f:
+                                    json.dump(feeds, f, indent=4)
+                                return i
                 return render_template('errors.html', error=Error_Post_Not_Found)
             return render_template('errors.html', error=Error_Wrong_ID_Int)
         return render_template('errors.html', error=Error_ID_Not_Input)
     def delete(self, id, userId):
         if id is not None:
-            with open('db.json', 'r') as f:
-                feeds = json.load(f)
-            posts = feeds['posts']
-            for i in posts:
-                if i['id'] == id:
-                    posts.remove(i)
-                    with open('db.json', 'w') as f:
-                        json.dump(feeds,f, indent=4)
-                    return "Post was succesfulyy deleted"
-            return render_template('errors.html', error=Error_Post_Not_Found)
+            if id.isnumeric:
+                id = int(id)
+                for j in Post.select():
+                    if j.id == id:
+                        post_to_delete = Post.get(id=id)
+                        post_to_delete.delete_instance()
+                        with open('db.json', 'r') as f:
+                            feeds = json.load(f)
+                        posts = feeds['posts']
+                        for i in posts:
+                            if i['id'] == id:
+                                posts.remove(i)
+                                with open('db.json', 'w') as f:
+                                    json.dump(feeds,f, indent=4)
+                                return "Post was succesfulyy deleted"
+                return render_template('errors.html', error=Error_Post_Not_Found)
+            return render_template('erros.html', error = Error_Wrong_ID_Int)
         elif userId is not None:
-            with open('db.json', 'r') as f:
-                feeds = json.load(f)
-            posts = feeds['posts']
-            for i in posts:
-                if i['userId'] == userId:
-                    posts.remove(i)
-                    with open('db.json', 'w') as f:
-                        json.dump(feeds, f, indent=4)
-                    return "Post was succesfulyy deleted"
-            return render_template('errors.html', error=Error_Post_Not_Found)
+            if userId.isnumeric:
+                userId = int(userId)
+                for j in Post.select():
+                    if j.userId == userId:
+                        post_to_delete = Post.get(userId=userId)
+                        post_to_delete.delete_instance()
+                        with open('db.json', 'r') as f:
+                            feeds = json.load(f)
+                        posts = feeds['posts']
+                        for i in posts:
+                            if i['userId'] == userId:
+                                posts.remove(i)
+                                with open('db.json', 'w') as f:
+                                    json.dump(feeds, f, indent=4)
+                                return "Post was succesfulyy deleted"
+                return render_template('errors.html', error=Error_Post_Not_Found)
+            return render_template('erros.html', error= Error_Wrong_ID_Int)
+        return render_template('erros.html', error = Error_ID_Not_Input)
 
 posts = Posts.as_view('posts')
 app.add_url_rule('/posts/', view_func=posts, methods = ['POST'])
